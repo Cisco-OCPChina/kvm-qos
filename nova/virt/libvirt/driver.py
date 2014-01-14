cd56e23fa38a08e562c81c6a34efaba9553a800a
iamjackhu <juhu2@cisco.com>
2014-01-14 10:05:18 +0800

    append core number

--- nova/virt/libvirt/driver.py
+++ nova/virt/libvirt/driver.py
@@ -3,12 +3,12 @@
     @exception.wrap_exception()
     def partition(self, hypervisor, component, instance_list, percentage):
         if cmp(component, 'cpu') == 0:
-            self._do_cpu_partition(hypervisor, instance_list, percentage)
-
-    def _do_cpu_partition(self, hypervisor, instance_list, percentage):
+            self._do_cpu_partition(instance_list, percentage)
+        
+    def _do_cpu_partition(self, instance_list, percentage):
         dirs = '/cgroup/cpu/libvirt/qemu/'
         for i in instance_list:
-            quotas = int(100000 * float(i['percentage']) )
+            quotas = int(100000 * float(i['percentage']) * int(i['vcpus']) )
             ipath = (dirs + i['instance_name'] + '/cpu.cfs_quota_us')
             utils.execute('chmod', '777', ipath, run_as_root=True)
             f = open(ipath, 'w')
